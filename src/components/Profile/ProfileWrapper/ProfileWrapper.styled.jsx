6eca29c78e244c820086b7843a372c17b65984f4
ViktorSavchenko <e_d_d.opk@ukr.net>
2023-03-02 17:51:30 +0200

    Bf

--- src/components/Profile/ProfileWrapper/ProfileWrapper.styled.jsx
+++ src/components/Profile/ProfileWrapper/ProfileWrapper.styled.jsx
@@ -87,31 +87,3 @@ export const StatsNumber = styled.span`
   font-size: 14px;
   line-height: 1.375;  
 `;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const ProfileContainer = styled.div`
-// margin-bottom: 40px;
-
-// width: 300px;
-
-// border-radius: 3px;
-// box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
-// 0px 2px 1px rgba(0, 0, 0, 0.2);
-
-// `;
